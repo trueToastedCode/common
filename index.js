2ca0e0dbdea8775707bcbd77953361e00a391e34
trueToastedCode <lennard2hp@gmail.com>
2023-04-23 19:38:10 +0200

    rm junk

--- index.js
+++ index.js
@@ -3,11 +3,3 @@ import jwt from 'jsonwebtoken'
 import buildMakeToken from './token'
 
 export default buildMakeToken({ jwt })
-
-
-Object.freeze({
-  sign: ({ expireAt, secret, data, algorithm = 'RS256' } = {}) => jwt.sign({
-    data, exp: expireAt ? Math.floor(expireAt / 1000) : undefined
-  }, secret, { algorithm }),
-  verify: ({ token, secret }) => jwt.verify(token, secret).data
-})
