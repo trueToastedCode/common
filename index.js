6b2fc5f400b0fa835fa84915568fbb67aebc5116
trueToastedCode <lennard2hp@gmail.com>
2023-05-08 17:33:14 +0200

    add copyRenameProperty

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-export default function renameProperty (obj, oldName, newName) {
+function renameProperty (obj, oldName, newName) {
   const value = obj[oldName]
   delete obj[oldName]
   obj[newName] = value
   return obj
 }
+
+function copyRenameProperty (obj, oldName, newName) {
+  return renameProperty({ ...obj }, oldName, newName)
+}
+
+const _renameProperty = Object.freeze({ copyRenameProperty, renameProperty })
+
+export default _renameProperty
+export { copyRenameProperty, renameProperty }
