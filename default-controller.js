e80ccd867469f8ecb793aa703cec10b1ce4f3cc0
trueToastedCode <lennard2hp@gmail.com>
2023-05-12 12:19:03 +0200

    add CustomError support

---
+++ default-controller.js
@@ -0,0 +1,17 @@
+export default function buildMakeDefaultController ({ CustomError }) {
+  return function makeDefaultController ({ ucFunc, statusCode = 200 }) {
+    return async function defaultController (info) {
+      try {
+        const result = await ucFunc(info)
+        return result == null
+          ? { statusCode }
+          : { statusCode, body: result }
+      } catch (e) {
+        return {
+          statusCode: e instanceof CustomError ? e.errorCode : 500,
+          error: e.message
+        }
+      }
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-export default function makeDefaultController ({ ucFunc, statusCode = 200 }) {
-  return async function defaultController (info) {
-    try {
-      const result = await ucFunc(info)
-      return result == null
-        ? { statusCode }
-        : { statusCode, body: result }
-    } catch (e) {
-      return {
-        statusCode: 500,
-        error: e.message
-      }
-    }
-  }
-}
+import CustomError from '../custom-error'
+
+import buildMakeDefaultController from './default-controller'
+
+export default buildMakeDefaultController({ CustomError })
