544d77407b3b9506200bc95c78b8b7551a108a01
trueToastedCode <lennard2hp@gmail.com>
2023-05-21 13:04:31 +0200

    rm getter methods

--- credentials.js
+++ credentials.js
@@ -4,8 +4,8 @@ export default function buildMakeCredentials ({ fs, path, parseCredentials }) {
       path.resolve(__dirname, credentialsPath))
     const credentials = parseCredentials(data)
     return Object.freeze({
-      getUsername: () => credentials.username,
-      getPassword: () => credentials.password
+      username: credentials.username,
+      password: credentials.password
     })
   }
 }
