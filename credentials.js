ec28ee127034872363e7a841e488ffaac49fb109
trueToastedCode <lennard2hp@gmail.com>
2023-05-21 13:20:40 +0200

    fix stuff

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 export default function buildMakeCredentials ({ fs, path, parseCredentials }) {
   return function makeCredentials ({ credentialsPath = '../../../credentials.txt' } = {}) {
     const data = fs.readFileSync(
-      path.resolve(__dirname, credentialsPath))
+      path.resolve(__dirname, credentialsPath)).toString()
     const credentials = parseCredentials(data)
     return Object.freeze({
       username: credentials.username,
